649a1836931b33404d92c51cf3e31cb1c40e9fb7
Andrew <gordiyko.andrey@gmail.com>
2023-05-18 16:21:24 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper">
+    <div class="_container">
+        <div class="crm">
+
+        </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<style scope>
+  .crm {
+    background: #fff;
+    width: 370px;
+    height: 812px;
+  }
+</style>
+
